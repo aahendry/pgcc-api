865a743cc83acb8b51999c50958b5f6457ba143a
aahendry <aahendry@hotmail.com>
2019-10-14 09:31:38 +0100

    Added Merch and Table sections.

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -19,7 +19,7 @@ namespace PgccApi.Services
 
     public class UserService : IUserService
     {
-        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        // TODO - users hardcoded for simplicity, store in a db with hashed passwords in production applications
         private List<User> _users = new List<User>
         { 
             new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" } 
